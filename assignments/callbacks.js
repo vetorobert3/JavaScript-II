09cfaa8fecc5ee0880f550d77e9eea654e58b7a4
Veto Ramirez <vetorobert3@gmail.com>
2019-08-10 10:18:08 -0700

    still need to do the question for the callback section.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -70,6 +70,7 @@ multiplyNums(8, 4, function(product) {
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb()
 }
 
 /* STRETCH PROBLEM */
